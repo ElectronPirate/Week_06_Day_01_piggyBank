731d5bd500000546eee067d240bd12682d532246
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-05-06 14:52:59 +0100

    piggy_bank

---
+++ src/PiggyBank.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+
+class PiggyBank extends Component {
+
+  constructor(props){
+
+    super(props)
+    this.state = {
+      total: 0
+    }
+
+    this.deposit = this.deposit.bind(this);
+    this.withdraw = this.withdraw.bind(this);
+
+  }
+
+  deposit(){
+    this.setState((prevState) => {
+    return {total: prevState.total + this.props.amount}
+    // this.setState({total: this.state.total + 1})
+
+  })
+}
+
+  withdraw(){
+
+    this.setState((prevState) => {
+      return {total: prevState.total - this.props.amount}
+
+    })
+  }
+
+  render(){
+
+
+  return (
+    <>
+    <h1>{this.props.title}</h1>
+    <p>Total: £{this.state.total}</p>
+    <button onClick={this.deposit}>Deposit</button>
+    <button onClick={this.withdraw}>Withdraw</button>
+    </>
+  )
+}
+}
+
+
+export default PiggyBank;
